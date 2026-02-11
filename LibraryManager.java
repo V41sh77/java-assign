074d2deb5714380fe8ddccda606392bd27b17462
Vaishnav Vijayan <v41sh7@gmail.com>
2026-02-10 23:03:57 -0500

    added choices

--- LibraryManager.java
+++ LibraryManager.java
@@ -21,10 +21,43 @@ public class LibraryManager {
 
             int choice = scanner.nextInt();
             scanner.nextLine(); 
+             if (choice == 1) {
 
-            if (choice == 3) {
+    System.out.print("Enter book title: ");
+    String title = scanner.nextLine();
+
+    System.out.print("Enter book author: ");
+    String author = scanner.nextLine();
+
+    System.out.print("Enter ISBN: ");
+    String isbn = scanner.nextLine();
+
+    // Create Book object and add to list
+    Book newBook = new Book(title, author, isbn);
+    bookList.add(newBook);
+
+    System.out.println("Book added successfully!");
+}
+else if (choice == 2) {
+
+                System.out.println("Total books right now: " + bookList.size()); 
+
+                if (bookList.isEmpty()) {
+                    System.out.println("No books available.");
+                } else {
+                    System.out.println("\n--- All Books ---");
+                    for (Book book : bookList) {
+                        book.displayBookInfo();
+                    }
+                }
+            }
+
+            else if (choice == 3) {
                 System.out.println("Exiting program");
                 break;
+            }
+            else{
+                 System.out.println("Invalid choice. Please enter 1, 2, or 3.");
             }
         }
 
