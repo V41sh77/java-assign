c82799f5fecad1d5d848c427061b2d16c62de588
Vaishnav Vijayan <v41sh7@gmail.com>
2026-02-10 21:55:12 -0500

    added constructor

--- Book.java
+++ Book.java
@@ -3,4 +3,17 @@ public class Book {
     private String bookAuthor;
     private String bookIsbn;
     private boolean isAvailable;  
+      
+    // Default constructor
+    public Book() {
+    this.bookTitle = "Unknown";
+    this.bookAuthor = "Unknown";
+    this.bookIsbn = "0000000000";
+    this.isAvailable = true;
 }
+    public Book(String title, String author, String isbn) {
+        setBookTitle(title);
+        setBookAuthor(author);
+        setBookIsbn(isbn);
+        this.isAvailable = true;
+    }
